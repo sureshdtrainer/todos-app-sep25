e3f0eca0d630986a3559701bc2508e926f915914
sureshdtrainer <suresh.d.trainer@gmail.com>
2025-09-04 07:44:30 +0000

    added add todo form

--- src/app/todo-add-edit/todo-add-edit.ts
+++ src/app/todo-add-edit/todo-add-edit.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-todo-add-edit',
-  imports: [MatDialogModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule, MatInputModule,
+    MatIconModule, MatDatepickerModule,
+    MatSelectModule,
+  ],
   templateUrl: './todo-add-edit.html',
   styleUrl: './todo-add-edit.css'
 })
